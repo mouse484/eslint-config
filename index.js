eb0fa21d3f117ce8fdfdc15915cd528d53c4e829
mouse_484 <escmouse484@gmail.com>
2019-12-05 03:11:49 +0000

    ➕ rule :no-multi-assign

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = {
         'prefer-const': ['error', { destructuring: 'all' }], //constの使用を提案する
         'no-var': 'error', //varを使わない
         'one-var': ['error', 'never'], //変数は一つず定義する
+        'no-multi-assign': 'error', //複数の割当の連鎖を禁止
 
         'no-new-object': 'error', //オブジェクトはリテラル({})を使用して作成する
         'object-shorthand': ['error', 'always'], //オブジェクトのショートハンド構文を使う
