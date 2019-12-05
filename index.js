78379a938b476360513caefd6e10d54923f7e397
mouse_484 <escmouse484@gmail.com>
2019-12-05 01:20:26 +0000

    ➕ rule : object-shorthand

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = {
         "prefer-const": ["error", { destructuring: "all" }], //constの使用を提案する
         "no-var": "error", //varを使わない
         "no-new-object": "error", //オブジェクトはリテラル({})を使用して作成する
+        "object-shorthand": ["error", "always"], //オブジェクトのショートハンド構文を使う
         camelcase: ["error", { properties: "never" }], //変数の命名をキャメルケース(camelCase)にする
     }
 };
