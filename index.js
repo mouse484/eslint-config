545fd3da56598090647b6b11eef891a056680da4
mouse_484 <escmouse484@gmail.com>
2019-12-05 02:41:52 +0000

    ➕ rule : implicit-arrow-linebreak

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ module.exports = {
         'arrow-parens': ['error', 'as-needed'], //アロー関数の()を必要に応じてつける
         'arrow-body-style': 'error', //アロー関数の{}を必要に応じてつける
         'no-confusing-arrow': 'error', //比較と混同される可能性のある矢印関数を禁止
+        'implicit-arrow-linebreak': 'error', //アロー関数の暗黙的な返り値を利用する場合の本体の位置
         
 
         camelcase: ['error', { properties: 'never' }], //変数の命名をキャメルケース(camelCase)にする
