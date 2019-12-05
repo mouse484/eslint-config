d8f25680ef72fe2eff478c984d5f58a76cf04940
mouse_484 <escmouse484@gmail.com>
2019-12-05 03:26:18 +0000

    ➕ rule : no-unneeded-ternary

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ module.exports = {
         'generator-star-spacing': ['error', { before: false, after: true }], //ジェネレータ関数の*の位置
 
         'no-nested-ternary': 'error', //ネストされた三項演算子の禁止
+        'no-unneeded-ternary': 'error', //不要な三項演算子の禁止
 
         camelcase: ['error', { properties: 'never' }], //変数の命名をキャメルケース(camelCase)にする
     }
