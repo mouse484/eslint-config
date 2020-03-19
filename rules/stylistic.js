67404bf7c46b98c076aaca9bbd7d5bbcf007e5a8
mouse_484 <escmouse484@gmail.com>
2020-03-19 08:40:22 +0000

    [add] array-bracket-spacing:error

--- rules/stylistic.js
+++ rules/stylistic.js
@@ -3,7 +3,10 @@ module.exports = {
     // https://eslint.org/docs/rules/array-bracket-newline
     'array-bracket-newline': ['error', 'consistent'],
 
-    // https://eslint.org/docs/rules/array-element-newline
+    // https://eslint.org/docs/rules/array-bracket-spacing
     'array-element-newline': ['error', { multiline: true, minItems: 3 }],
+
+    // https://eslint.org/docs/rules/array-element-newline
+    'array-bracket-spacing': ['error', 'never'],
   }
 }
