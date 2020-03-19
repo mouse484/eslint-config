e1503a12c424644a0ecac3135147943aeb0f2c9c
mouse_484 <escmouse484@gmail.com>
2020-03-19 08:51:31 +0000

    [add] comma-dangle:error
    always-multiline

--- rules/stylistic.js
+++ rules/stylistic.js
@@ -31,5 +31,14 @@ module.exports = {
         ignoreConsecutiveComments: true,
       },
     }],
+
+    // https://eslint.org/docs/rules/comma-dangle
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
   }
 }
