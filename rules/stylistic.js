f864bd70f59c842a1fa32bcdebbfc4a493e15ad0
mouse_484 <escmouse484@gmail.com>
2020-03-19 08:38:49 +0000

    [add] array-element-newline:error
    { multiline: true, minItems: 3 }

--- rules/stylistic.js
+++ rules/stylistic.js
@@ -2,5 +2,8 @@ module.exports = {
   rules: {
     // https://eslint.org/docs/rules/array-bracket-newline
     'array-bracket-newline': ['error', 'consistent'],
+
+    // https://eslint.org/docs/rules/array-element-newline
+    'array-element-newline': ['error', { multiline: true, minItems: 3 }],
   }
 }
