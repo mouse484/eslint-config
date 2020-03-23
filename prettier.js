16c44ad841d24d05a4d82d3df7942da8b310e9be
mouse_484 <escmouse484@gmail.com>
2020-03-23 05:23:03 +0000

    fix prettier

--- prettier.js
+++ prettier.js
@@ -1,18 +1,5 @@
 'use strict';
-
-const prettier = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  arrowParens: 'always',
-  vueIndentScriptAndStyle: true,
-  endOfLine: 'lf'
-}
+const prettier = require("./.prettierrc.js")
 
 module.exports = {
   plugins: ['prettier'],
@@ -20,5 +7,4 @@ module.exports = {
   rules: {
     'prettier/prettier': ['error', prettier]
   },
-  prettier
 };
